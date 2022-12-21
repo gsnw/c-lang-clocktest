d33eb8be939956c857a8b3fddacf4bf8af759f57
German Service Network <support@gsnw.de>
2022-12-21 10:50:13 +0100

    Optimization and correction of CLOCK_MONOTONIC in all three function calls

--- clocktest.c
+++ clocktest.c
@@ -4,18 +4,23 @@
 #include <stddef.h>
 #include <unistd.h>
 
+#define CLOCK_MONOTONIC_NAME "CLOCK_MONOTONIC"
+#define CLOCK_REALTIME_NAME "CLOCK_REALTIME"
+#define CLOCK_UPTIME_NAME "CLOCK_UPTIME"
+
 static void printClock(char *clockIdName, clockid_t CLOCK_ID)
 {
     struct timespec curr_tm;
     clock_gettime(CLOCK_ID, &curr_tm);
-    printf("[%s] Seconds: %ld\n", clockIdName, curr_tm.tv_sec);
-    printf("[%s] Nanoseconds: %ld\n", clockIdName, curr_tm.tv_nsec);
+    printf("[%s] Seconds: %ld Nanoseconds: %ld\n", clockIdName, curr_tm.tv_sec, curr_tm.tv_nsec);
 }
 
 int main(void)
 {
-    printClock("CLOCK_MONOTONIC", CLOCK_MONOTONIC);
-    printClock("CLOCK_REALTIME", CLOCK_MONOTONIC);
-    printClock("CLOCK_UPTIME", CLOCK_MONOTONIC);
+    printClock(CLOCK_MONOTONIC_NAME, CLOCK_MONOTONIC);
+    printClock(CLOCK_REALTIME_NAME, CLOCK_REALTIME);
+#if defined(BSD)
+    printClock(CLOCK_UPTIME_NAME, CLOCK_UPTIME);
+#endif
     return EXIT_SUCCESS;
 }
